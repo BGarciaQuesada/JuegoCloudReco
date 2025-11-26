ff3b4a1cb4d799de113b6cb84804eb975679ab47
Belen Garcia Quesada <bgarque830@g.educaand.es>
2025-11-26 12:25:36 +0100

    commit inicial

---
+++ Assets/Scripts/SimpleCloudRecoEventHandler1.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using Vuforia;
+
+
+public class MetaDatos
+{
+    public string name;
+    public string nFragment;
+    public string url;
+
+    public static MetaDatos CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<MetaDatos>(jsonString);
+    }
+
+}
+
+public class SimpleCloudRecoEventHandler1 : MonoBehaviour
+{
+    CloudRecoBehaviour mCloudRecoBehaviour;
+    bool mIsScanning = false;
+    string mTargetMetadata = "";
+    MetaDatos metaDatosVuforia;
+
+    public ImageTargetBehaviour ImageTargetTemplate;
+
+    void Start()
+    {
+    }
+    // Register cloud reco callbacks
+    void Awake()
+    {
+        mCloudRecoBehaviour = GetComponent<CloudRecoBehaviour>();
+        mCloudRecoBehaviour.RegisterOnInitializedEventHandler(OnInitialized);
+        mCloudRecoBehaviour.RegisterOnInitErrorEventHandler(OnInitError);
+        mCloudRecoBehaviour.RegisterOnUpdateErrorEventHandler(OnUpdateError);
+        mCloudRecoBehaviour.RegisterOnStateChangedEventHandler(OnStateChanged);
+        mCloudRecoBehaviour.RegisterOnNewSearchResultEventHandler(OnNewSearchResult);
+    }
+    //Unregister cloud reco callbacks when the handler is destroyed
+    void OnDestroy()
+    {
+        mCloudRecoBehaviour.UnregisterOnInitializedEventHandler(OnInitialized);
+        mCloudRecoBehaviour.UnregisterOnInitErrorEventHandler(OnInitError);
+        mCloudRecoBehaviour.UnregisterOnUpdateErrorEventHandler(OnUpdateError);
+        mCloudRecoBehaviour.UnregisterOnStateChangedEventHandler(OnStateChanged);
+        mCloudRecoBehaviour.UnregisterOnNewSearchResultEventHandler(OnNewSearchResult);
+    }
+    public void OnInitialized(CloudRecoBehaviour cloudRecoBehaviour)
+    {
+        Debug.Log("Cloud Reco initialized");
+    }
+
+    public void OnInitError(CloudRecoBehaviour.InitError initError)
+    {
+        Debug.Log("Cloud Reco init error " + initError.ToString());
+    }
+
+    public void OnUpdateError(CloudRecoBehaviour.QueryError updateError)
+    {
+        Debug.Log("Cloud Reco update error " + updateError.ToString());
+    }
+    public void OnStateChanged(bool scanning)
+    {
+        mIsScanning = scanning;
+
+        if (scanning)
+        {
+            // Clear all known targets
+        }
+    }
+
+    // Here we handle a cloud target recognition event
+    public void OnNewSearchResult(CloudRecoBehaviour.CloudRecoSearchResult cloudRecoSearchResult )
+    {
+        // Store the target metadata
+        mTargetMetadata = cloudRecoSearchResult.TargetName;
+        metaDatosVuforia = MetaDatos.CreateFromJSON(cloudRecoSearchResult.MetaData);
+        StartCoroutine(GetAssetBundle(metaDatosVuforia.url));
+
+        if (ImageTargetTemplate)
+        {
+            /* Enable the new result with the same ImageTargetBehaviour: */
+            mCloudRecoBehaviour.EnableObservers(cloudRecoSearchResult, ImageTargetTemplate.gameObject);
+        }
+
+        // Stop the scanning by disabling the behaviour
+        mCloudRecoBehaviour.enabled = false;
+    }
+    void OnGUI() {
+        // Display current 'scanning' status
+        GUI.Box (new Rect(100,100,200,50), mIsScanning ? "Scanning" : "Not scanning");
+        // Display metadata of latest detected cloud-target
+        GUI.Box (new Rect(100,200,200,50), "Metadata: " + metaDatosVuforia.url);
+        // If not scanning, show button
+        // so that user can restart cloud scanning
+        if (!mIsScanning) {
+            if (GUI.Button(new Rect(100,300,200,50), "Restart Scanning")) {
+                // Reset Behaviour
+                mCloudRecoBehaviour.enabled = true;
+                mTargetMetadata="";
+            }
+        }
+    }
+
+    IEnumerator GetAssetBundle(string url)
+    {
+        UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle(metaDatosVuforia.url);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(www);
+            string[] allAssetNames = bundle.GetAllAssetNames();
+            string gameObjectName = Path.GetFileNameWithoutExtension(allAssetNames[0]).ToString();
+            GameObject objectFound = bundle.LoadAsset(gameObjectName) as GameObject;
+            Instantiate(objectFound, ImageTargetTemplate.transform);
+
+        }
+    }
+}
